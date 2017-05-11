813f18036ab37cf02bee0c3c65f26b36ef24e354
Kolja Hopfmann <k.hopfmann@hotmail.de>
2017-05-11 20:42:02 +0200

    impl linedrawer

---
+++ src/MyDraw/LineDrawer.java
@@ -0,0 +1,34 @@
+package MyDraw;
+
+import java.awt.*;
+
+/**
+ * Created by khopf on 11/05/2017.
+ */
+public class LineDrawer implements Drawable
+{
+    public Point startPoint;
+    public Point endPoint;
+
+    public LineDrawer(Point start, Point end)
+    {
+        startPoint = start;
+        endPoint = end;
+    }
+
+    @Override
+    public void draw(Graphics g)
+    {
+        if(startPoint.x < 0 && startPoint.y < 0)
+        {
+            return;
+        }
+
+        int x1 = (int) startPoint.getX();
+        int y1 = (int) startPoint.getY();
+        int x2 = (int) endPoint.getX();
+        int y2 = (int) endPoint.getY();
+
+        g.drawLine(x1, y1, x2, y2);
+    }
+}
